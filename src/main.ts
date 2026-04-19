c581d6cc173498f991ac3e0b34aca694059a2143
Janeth Mar <ti46481@uvp.edu.mx>
2026-04-18 18:36:50 -0600

    Subida de archivos pdf, los manifiestos

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // CRÍTICO: Habilitar CORS primero para permitir peticiones web
   app.enableCors();
@@ -15,7 +17,12 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
 
-  // 2. Configuración de Swagger
+  // 2. Carpeta 'uploads' como directorio público estático
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  // 3. Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('LOGYMEX Ambiental API')
     .setDescription('Sistema de Gestión de Residuos Peligrosos y Bitácoras de Recolección')
@@ -26,8 +33,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
+  // 4. Inicialización del servidor
   await app.listen(3000);
   console.log(`Servidor corriendo en: http://localhost:3000`);
   console.log(`Documentación en: http://localhost:3000/api/docs`);
 }
+
+// Ejecución de la función principal
 bootstrap();
