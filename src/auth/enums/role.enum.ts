facb7f2f6661109e3559e6e8e11413b363e5f797
ElenaMar24 <ti46481@uvp.edu.mx>
2026-04-25 22:25:03 -0600

    chore: limpieza total para inicializar nueva arquitectura NestJS

--- src/auth/enums/role.enum.ts
+++
@@ -1,6 +0,0 @@
-// Enums de Roles (Director, Jefe, Operador)
-export enum Role {
-    OPERADOR = 'operador',
-    JEFE_LOGISTICA = 'jefe_logistica',
-    DIRECTOR_GENERAL = 'director_general',
-}
--- src/documents/dto/create-document.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateDocumentDto {
-    @ApiProperty({ description: 'Nombre descriptivo del documento para mostrar en la app' })
-    @IsString()
-    @IsNotEmpty({ message: 'Debe proporcionar un nombre para identificar el documento en la aplicación.' })
-    name: string;
-}
