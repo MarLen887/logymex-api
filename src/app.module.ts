d1304eb6ac90baae69bc3681e62e07b20e7bab2b
ElenaMar24 <ti46481@uvp.edu.mx>
2026-04-25 23:16:46 -0600

    feat: estructura de 8 módulos completada (Logymex API)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WasteModule } from './waste/waste.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { UnitsModule } from './units/units.module';
+import { LogsModule } from './logs/logs.module';
+import { FilesModule } from './files/files.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, WasteModule, InventoryModule, UnitsModule, LogsModule, FilesModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
